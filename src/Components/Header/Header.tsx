9aad314c2a08f9b82cc8b4b115dd8665dcaddc14
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-02-17 21:32:45 +0300

    chore: some preparing and clean up for future task

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,17 +1,17 @@
-import s from './Header.module.css'
-import headerLogo from '../../assets/vkLogo.svg'
+import s from "./Header.module.css";
+import futureLogo from "../.././../public/futureLogo.svg";
 
- const Header = () => {
+const Header = () => {
   return (
     <header>
-        <div className={s.wrapper}>
-            <div className={s.title__block}>
-                <h1 className={s.title}>Hi VK!</h1>
-                <img src={headerLogo} alt='VK logo' />
-            </div>
+      <div className={s.wrapper}>
+        <div className={s.title__block}>
+          <h1 className={s.title}>Hi future!</h1>
+          <img className={s.logo} src={futureLogo} alt="future logo" />
         </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
