300ea1237eb2f8ee822f916e83acd6fff7a0386c
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-02-18 22:33:02 +0300

    feat: added authorization via github

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,11 @@
 import s from "./Header.module.css";
 import futureLogo from "../.././../public/futureLogo.svg";
+import { LogoutOptions, useAuth0 } from "@auth0/auth0-react";
+import { Button } from "antd";
 
 const Header = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
   return (
     <header>
       <div className={s.wrapper}>
@@ -9,6 +13,27 @@ const Header = () => {
           <h1 className={s.title}>Hi future!</h1>
           <img className={s.logo} src={futureLogo} alt="future logo" />
         </div>
+        {!isAuthenticated ? (
+          <Button
+            color="cyan"
+            variant="solid"
+            onClick={() => loginWithRedirect()}
+          >
+            Login
+          </Button>
+        ) : (
+          <>
+            <Button
+              color="cyan"
+              variant="outlined"
+              onClick={() =>
+                logout({ returnTo: window.location.origin } as LogoutOptions)
+              }
+            >
+              Logout
+            </Button>
+          </>
+        )}
       </div>
     </header>
   );
